3f0ab48f30e7fe65c7bbbb2db21dd687086b2514
lambdathejojo <dominic.castillo.pms@outlook.com>
2024-06-07 08:07:43 -0600

    Inicio

---
+++ Inicioambos.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Inicioambos.css">
+    <title>Inicio de Sesión</title>
+</head>
+<body>
+    
+    <div class="container">
+        <h1>Inicio de Sesión</h1>
+            <br><br>
+        <form action="">
+
+            <div class="form-group">
+                <label for="name">Nombre Completo:</label>
+                <input type="text" id="name">
+            </div>
+                <br>
+
+            <div class="form-group">
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password">
+            </div>
+                <br><br>
+            <center>
+            <a href="" class="forgot-password">¿Olvidaste tu contraseña?</a>
+            <button type="submit" class="btn btn-primary">Iniciar sesión</button>    
+            </center>
+            
+        </form>
+    
+            <br>
+        <div class="separator"><span></span></div>
+            <br>
+
+        <a href="https://www.google.com" class="btn btn-google">Google</a>
+        <br>
+    </div>
+
+</body>
+</html>
